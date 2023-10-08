2f19777582f0d6e410c90de76a1982d1e5af573e
saksham2026 <sakshamiitroorkee3@gmail.com>
2023-10-08 12:07:04 +0530

    adding components for User page

---
+++ rushlearn/src/components/Navbar2.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import '/home/techsavvy/RushLearn/rushlearn/src/App.css'
+
+function NavbarUser(){
+    return(
+        <>
+        
+        <header>
+        <nav>
+            <div class="logo">
+                <img src="logo.png" alt="Your Logo"/>
+                <h1>Your Study App</h1>
+            </div>
+            <div class="profile">
+                <img src="profile.png" alt="User Profile"/>
+            </div>
+        </nav>
+    </header>
+        </>
+    )
+}
+
+export default NavbarUser;
---
+++ rushlearn/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import '/home/techsavvy/RushLearn/rushlearn/src/App.css'
+
+function Footer(){
+    <>
+    <footer>
+        <p>&copy; 2023 Your Study App</p>
+    </footer>
+    </>
+}
+export default Footer ;
