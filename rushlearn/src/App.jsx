88c0afe751131774a0771ee900f440909822a245
Vaibhav Singh <the_vaibhav@icloud.com>
2023-10-08 20:52:16 +0530

    nothing much

--- rushlearn/src/App.jsx
+++ rushlearn/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import SignIn from './pages/SignIn'
-import NavbarUser from '/Users/vaibhavsingh/RushLearn/rushlearn/src/components/Navbar2.jsx'
 import './App.css'
 import UserMain from './components/UserMain'
 import Footer from './components/footer'
@@ -10,7 +9,6 @@ export default function App() {
   return (
     <div>
       {/* <SignIn/> */}
-      {/* <NavbarUser/> */}
       <Navbar/>
       <UserMain/>
       <Footer/>
